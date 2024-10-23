593bc42d3ff2ea3a871caf5a3c79843a14d9ddff
arnau.nau <arnau.sanz@gmail.com>
2024-10-24 00:15:31 +0200

    Lab S4 src

--- S4/src/S4.c
+++ S4/src/S4.c
@@ -19,6 +19,15 @@
 #include <string.h>
 #include <stdio.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define TTY_COLOR_DEFAULT "\033[m"
+#define TTY_COLOR_MAGENTA "\033[35m"
+#define TTY_COLOR_BLUE "\033[34m"
+#define TTY_COLOR_YELLOW "\033[33m"
+#define TTY_COLOR_GREEN "\033[32m"
+#define TTY_COLOR_RED "\033[31m"
 
 void ess_print(const char * const string) {
     write(STDOUT_FILENO, string, strlen(string));
@@ -80,54 +89,138 @@ char * ess_read_line(const int fd) {
 }
 
 // 9700-9709
-#define SERVER_PORT 9705
+#define REQUEST_CHALLENGE 1
+#define SEND_ANSWER 2
+#define REQUEST_HINT 3
+#define CURRENT_STATUS 4
+#define TERMINATE 5
 
-int main () {
 
-    const int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (socket_fd < 0) {
-        ess_print_error("socket TCP");
+int sockfd; //so I can close the socket on ctrl+C <3
+
+void print_menu() {
+    ess_println("====================================");
+    ess_println("||\t\tNavigation Menu\t\t||");
+    ess_println("====================================");
+    ess_println("1. Receive Current Challenge");
+    ess_println("2. Send Response to Challenge");
+    ess_println("3. Request Hint");
+    ess_println("4. View Current Mission");
+    ess_println("5. Terminate Connection and Exit");
+    ess_print("Select an option: ");
+}
+
+void receive_message(const int sock, char **buffer) {
+    *buffer = ess_read_line(sock);
+    if (*buffer == NULL) {
+        close(sock);
+        exit(-1);
+    }
+    ess_println(*buffer);
+}
+
+void send_message(const int sock, const char *const string) {
+    char *buff = malloc(sizeof(char) * strlen(string)+2);
+    strcpy(buff, string);
+    buff[strlen(string)] = '\n';
+    buff[strlen(string)+1] = '\0';
+    write(sock, buff, strlen(buff));
+    free(buff);
+}
+
+void ignore_sigint() {
+    ess_println("Ignoring ctrl+C.");
+}
+
+int main (const int argc, char *argv[]) {
+    signal(SIGINT, ignore_sigint);
+
+    if (argc != 3) {
+        ess_print_error("Wrong number of arguments.");
+        ess_println("Usage: ./client <server_ip> <port>");
         exit(EXIT_FAILURE);
+    }
+
+    struct in_addr ip_addr;
+    inet_aton(argv[1], &ip_addr);
+    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sockfd < 0) {
+        ess_print_error("socket");
+        exit (EXIT_FAILURE);
     }
 
     struct sockaddr_in s_addr;
-    memset(&s_addr, 0, sizeof (s_addr));
+    memset(&s_addr, 0, sizeof(s_addr));
     s_addr.sin_family = AF_INET;
-    s_addr.sin_port = htons(9705);
-    s_addr.sin_addr.s_addr = INADDR_ANY;
+    s_addr.sin_port = htons(atoi(argv[2]));
+    s_addr.sin_addr = ip_addr;
+    if(connect (sockfd, (void *) &s_addr, sizeof (s_addr)) < 0){
+        ess_print_error("connect");
+        exit (EXIT_FAILURE);
 
-    if (bind (socket_fd, (void *) &s_addr, sizeof (s_addr)) < 0) {
-        ess_print_error("Unable to bind");
-        exit(EXIT_FAILURE);
     }
+    ess_println( TTY_COLOR_MAGENTA "Welcome to RiddleQuest. Prepare to unlock the secrets and discover the treasure!" TTY_COLOR_DEFAULT);
 
-    listen (socket_fd, 5);
+    ess_print("Please enter your username: ");
+    char *buffer = ess_read_line(STDIN_FILENO);
+    send_message(sockfd, buffer);
+    free(buffer);
+
+    /*buffer = ess_read_line(sockfd);
+    ess_println(buffer);*/
 
     while (1) {
-        struct sockaddr incoming_addr;
-        socklen_t incoming_len = sizeof(incoming_addr);
+        print_menu();
+        buffer = ess_read_line(STDIN_FILENO);
+        const int option = atoi(buffer);
 
-        const int new_socket = accept(socket_fd, &incoming_addr, &incoming_len);
-        if (new_socket < 0) {
-            ess_print_error("error accepting");
-            exit(EXIT_FAILURE);
+        send_message(sockfd, buffer);
+        free(buffer);
+
+        switch (option) {
+            case REQUEST_CHALLENGE:
+                //request challenge
+                    ess_print(TTY_COLOR_MAGENTA "Challenge: " TTY_COLOR_DEFAULT);
+                    receive_message(sockfd, &buffer);
+                    free(buffer);
+                    ess_println("");
+                    break;
+            case SEND_ANSWER:
+                //send solution
+                    ess_print(TTY_COLOR_MAGENTA "Enter your response to the challenge: " TTY_COLOR_DEFAULT);
+                    buffer = ess_read_line(STDIN_FILENO);
+                    send_message(sockfd, buffer);
+                    free(buffer);
+                    receive_message(sockfd, &buffer);
+                    free(buffer);
+                    break;
+            case REQUEST_HINT:
+                //request hint
+                    ess_println(TTY_COLOR_MAGENTA "Your hint is coming..." TTY_COLOR_DEFAULT);
+                    receive_message(sockfd, &buffer);
+                    free(buffer);
+                    ess_println("");
+                    break;
+            case CURRENT_STATUS:
+                //current status
+                    ess_println(TTY_COLOR_MAGENTA "Current mission status:" TTY_COLOR_DEFAULT);
+                    receive_message(sockfd, &buffer);
+                    free(buffer);
+                    ess_println("");
+                    break;
+            case TERMINATE:
+                    ess_println(TTY_COLOR_YELLOW "Terminating connection..." TTY_COLOR_DEFAULT);
+                    close(sockfd);
+                    exit(EXIT_SUCCESS);
+                    break;
+            default:
+                    receive_message(sockfd, &buffer);
+                    free(buffer);
+                    ess_println("");
+                break;
         }
-        printf("accepted connection.\n");
-
-        ssize_t length;
-        char test_string[21];
-        memset(test_string, '\0', sizeof(char) * 21);
-
-        do {
-            length = read(new_socket, test_string, 20);
-            printf("read %ld bytes.\n", length);
-            ess_print("received: ");
-            ess_println(test_string);
-        } while (length > 0);
-        close(new_socket);
-
     }
-    close(socket_fd);
 
+    close(sockfd);
     exit(EXIT_SUCCESS);
 }
